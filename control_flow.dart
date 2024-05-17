d588d764926bff1e79e359495e7331325aa59c85
Cyril Odendo <162698745+cyril-jr@users.noreply.github.com>
2024-05-17 18:56:08 +0300

    Add files via upload

---
+++ control_flow.dart
@@ -0,0 +1,12 @@
+void main() {
+ int numb=9;
+  if (numb>10) {
+    print("Your number is greater than 10");
+  } else if(numb==10) {
+    print("Your number is equal to 10");
+  }
+  else {
+    print("Your number is less than 10");
+  }
+  
+}
